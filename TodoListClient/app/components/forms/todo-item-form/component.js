e43b2d1cd820bd6f46908b84ed908e651e8e19aa
Ryan Kennedy <rkennedy9064@gmail.com>
2021-07-24 16:30:40 -0500

    Added ember-changeset-validations

--- TodoListClient/app/components/forms/todo-item-form/component.js
+++ TodoListClient/app/components/forms/todo-item-form/component.js
@@ -1,18 +1,39 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import Changeset from 'ember-changeset';
+import lookupValidator from 'ember-changeset-validations';
+import TodoItemValidations from '../../../validations/todo-item';
 
 export default class TodoItemForm extends Component {
   @service notify;
   @service router;
 
+  constructor() {
+    super(...arguments);
+
+    // Was unable to use the changeset helpr, so I'm constructing the changeset manually.
+    // Most likely related to this issue, but the fix didn't work.
+    // https://github.com/poteto/ember-changeset-validations/issues/214
+    this.changeset = new Changeset(
+      this.args.todoItem,
+      lookupValidator(TodoItemValidations),
+      TodoItemValidations
+    );
+  }
+
   @action
-  async save(todoItem, event) {
+  async save(event) {
     event.preventDefault();
     try {
-      await todoItem.save();
-      this.router.transitionTo('s.todo-items');
+      await this.changeset.validate();
+
+      if (this.changeset.isValid) {
+        await this.changeset.save();
+        this.router.transitionTo('s.todo-items');
+      }
     } catch (error) {
+      console.log(error);
       this.notify.error('Failed to create item');
     }
   }
--- TodoListClient/app/models/todo-item.js
+++ TodoListClient/app/models/todo-item.js
@@ -1,16 +1,6 @@
 import Model, { belongsTo, attr } from '@ember-data/model';
-import { validator, buildValidations } from 'ember-cp-validations';
 
-const Validations = buildValidations({
-  description: [
-    validator('presence', true),
-    validator('length', {
-      min: 4,
-    }),
-  ],
-});
-
-export default Model.extend(Validations, {
-  description: attr('string'),
-  owner: belongsTo('person'),
-});
+export default class TodoItem extends Model {
+  @attr description;
+  @belongsTo person;
+}
