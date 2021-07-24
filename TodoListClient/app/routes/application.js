e037050ee8864edb7845d081b29d852d5b9074fd
Ryan Kennedy <rkennedy9064@gmail.com>
2021-07-24 18:36:16 -0500

    Cleanup deprecation

--- TodoListClient/app/routes/application.js
+++ TodoListClient/app/routes/application.js
@@ -1,11 +1,11 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 
 export default class ApplicationRoute extends Route {
   @service session;
+  @service router;
 
   beforeModel(/* transition */) {
-    this.transitionTo('s.todo-items');
+    this.router.transitionTo('s.todo-items');
   }
 }
